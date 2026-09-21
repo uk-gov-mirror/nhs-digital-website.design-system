ccd733150544dbd5ad565463eb482905baed86e7
geoffreyhayward <geoffrey.hayward@nhs.net>
2026-09-21 12:14:53 +0100

    build: remove legacy image minifier

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -1,17 +1,10 @@
-/* global ENV PATHS */
+/* global PATHS */
 
 const gulp = require('gulp');
-const gulpIf = require('gulp-if');
 const newer = require('gulp-newer');
-const { finished } = require('stream/promises');
 
 const getDestPath = () => PATHS.dist.images;
 
-gulp.task('images:copy', async () => {
-  const { default: imagemin } = await import('gulp-imagemin');
-  const stream = gulp.src(`${PATHS.src.images}/**/*`)
+gulp.task('images:copy', () => gulp.src(`${PATHS.src.images}/**/*`)
     .pipe(newer(getDestPath()))
-    .pipe(gulpIf(ENV.isModeProd(), imagemin()))
-    .pipe(gulp.dest(getDestPath()));
-  await finished(stream);
-});
+    .pipe(gulp.dest(getDestPath())));
