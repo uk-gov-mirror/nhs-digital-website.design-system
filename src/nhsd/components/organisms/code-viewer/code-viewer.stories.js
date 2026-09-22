83294b7d0404536ce1029d7edd75f562bd018d40
geoffreyhayward <geoffrey.hayward@nhs.net>
2026-09-22 14:21:07 +0100

    fix: initialize code viewer copy buttons

--- src/nhsd/components/organisms/code-viewer/code-viewer.stories.js
+++ src/nhsd/components/organisms/code-viewer/code-viewer.stories.js
@@ -21,9 +21,16 @@ const sourceCode = `// Sass import \n@use "nhsd/components/organisms/code-viewer
 
 // HTML`;
 
+const initializeCodeViewer = (Story) => {
+  const markup = Story();
+  setTimeout(() => globalThis.nhsd?.init(), 0);
+  return markup;
+};
+
 // Component defaults
 export default {
   title: "Design System / Components / Organisms / Code viewer",
+  decorators: [initializeCodeViewer],
   parameters: {
     docs: {
       description: {
