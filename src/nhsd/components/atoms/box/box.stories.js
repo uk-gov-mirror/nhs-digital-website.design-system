0546853592aa4cacba1b3d0029e0c8b9b05c8e90
geoffreyhayward <geoffrey.hayward@nhs.net>
2026-09-22 11:00:45 +0100

    fix: restore box story background contrast

--- src/nhsd/components/atoms/box/box.stories.js
+++ src/nhsd/components/atoms/box/box.stories.js
@@ -42,6 +42,19 @@ LabComponent.parameters = {
 
 export const WhiteBox = Template.bind({});
 WhiteBox.storyName = 'Background / White';
+WhiteBox.globals = {
+  backgrounds: {
+    value: '#425563',
+  },
+};
+WhiteBox.decorators = [
+  (Story, context) => {
+    const bleed = context.viewMode === 'docs'
+      ? 'box-shadow: 0 0 0 32px #425563; margin-top: -22px; margin-bottom: -22px;'
+      : '';
+    return `<div style="background-color: #425563; padding: 22px; box-sizing: border-box; ${bleed}">${Story()}</div>`;
+  },
+];
 WhiteBox.parameters = {
   backgrounds: {
     default: 'dim',
